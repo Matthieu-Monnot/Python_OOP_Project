b914f9ed86f3850a79239a4965d5aeb7019f6d63
Matthieu <matthieumonnot@yahoo.fr>
2023-10-24 19:58:34 +0200

    build app

---
+++ app.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Category(Enum):
+    TOOLS = 'tools'
+    CONSUMABLES = 'consumables'
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    count: int
+    id: int
+    category: Category
+
+
---
+++ main.py
@@ -0,0 +1,9 @@
+items = {
+    0: Item(name="Hammer", price=9.99, count=20, id=0, category=Category.TOOLS),
+    1: Item(name="Pliers", price=5.99, count=20, id=1, category=Category.TOOLS),
+    2: Item(name="Nails", price=1.99, count=100, id=2, category=Category.CONSUMABLES),
+}
+
+@app.get("/")
+def index() -> dict[str, dict[int, Item]]:
+    return {"items": items}
