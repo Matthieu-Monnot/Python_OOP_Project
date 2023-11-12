208d1055ed5e1aebbade6d52ac071f686c4f6002
Matthieu <matthieumonnot@yahoo.fr>
2023-11-12 19:49:43 +0100

    handle list, objects and json parameters

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, APIRouter
-import ast
+from fastapi import FastAPI, APIRouter, Query
+from typing import Annotated
+from pydantic import BaseModel
 
 
 my_router = APIRouter()
 app = FastAPI()
+request_count = 0
 
 
 def fast_api_decorator(route, method, type_args):
@@ -14,6 +16,10 @@ def fast_api_decorator(route, method, type_args):
                 if not isinstance(value, expected_type):
                     raise TypeError(f"Type d'argument incorrect. Attendu : {expected_type.__name__}, Reçu : {type(value).__name__}")
 
+            # Count the number of request
+            global request_count
+            request_count += 1
+
             # add endpoint to the API
             my_router.add_api_route(path=route, endpoint=func, methods=method)
             app.include_router(my_router)
@@ -22,27 +28,44 @@ def fast_api_decorator(route, method, type_args):
     return decorator
 
 
-@fast_api_decorator(route="/power/", method=["GET"], type_args=[str, str])
-def power_function(x: str, a: str):
-    return {f"{x} to the power of {a}": int(x)**int(a)}
+@fast_api_decorator(route="/power/", method=["GET"], type_args=[int, int])
+def power_function(x: Annotated[int, Query(description="Int we'll compute the power")], a: Annotated[int, Query(description="Power of the calculation")]):
+    return {f"{x} to the power of {a}": x ** a}
 
 
-@fast_api_decorator(route="/add/", method=["GET"], type_args=[str, str])
-def add_function(x: str, a: str):
-    return {f"{x} + {a} equals": int(x) + int(a)}
+@fast_api_decorator(route="/add/", method=["GET"], type_args=[int, int])
+def add_function(x: Annotated[int, Query(description="Int we'll add something")], a: Annotated[int, Query(description="Int added")]):
+    return {f"{x} + {a} equals": x + a}
 
 
-@fast_api_decorator(route="/sous/", method=["GET"], type_args=[str, list])
-def sous_function(x, lst):
-    x = ast.literal_eval(x)
-    # lst = ast.literal_eval(lst)
+@fast_api_decorator(route="/sous/", method=["GET"], type_args=[int, list])
+def sous_function(x: Annotated[int, Query(description="Int we'll substract something")], lst: Annotated[list[int], Query(description="List of 2 int that will be substracted")]):
     return {f"{x} - {lst[0]} - {lst[1]} equals": x - lst[0] - lst[1]}
 
 
+class InputDiv(BaseModel):
+    div: int
+
+
+# Pour faire une requête avec un argument "Body" ou un json avec des arguments il faut passer
+# par une méthode "POST" et pas "GET"
+@fast_api_decorator(route="/div/", method=["POST"], type_args=[int, InputDiv])
+def div_function(x: Annotated[int, Query(description="Int we will divide something")], item: InputDiv):
+    return {f"{x} / {item.div} equals": item.div}
+
+
+@app.get("/stats")
+async def get_stats():
+    global request_count
+    return {"request_count": request_count}
+
+
 # On "lance" les fonctions pour qu'elles soient lisibles par l'app FastAPI
-power_function(x="0", a="0")
-add_function(x="0", a="0")
-sous_function(x="0", lst=[0, 0])
+power_function(x=0, a=0)
+add_function(x=0, a=0)
+sous_function(x=0, lst=[0, 0])
+input_item = InputDiv(div=10)
+div_function(x=100, item=input_item)
 
 # résolution pb de lancement des fonctions
 """
--- main.py
+++ main.py
@@ -32,13 +32,13 @@ def add(x, a):
 
 
 def sous(x, lst):
-    print(requests.get(f"http://127.0.0.1:8000/sous/?x={x}&lst=[{lst[0]},{lst[1]}]").json())
+    print(requests.get(f"http://127.0.0.1:8000/sous/?x={x}&lst={lst[0]}&lst={lst[1]}").json())
 
 
 if __name__ == "__main__":
     power(x=9, a=2)
     add(x=9, a=2)
-    #sous(x=9, lst=[2, 1])
-    print(power_function(x="9", a="2"))
-    print(add_function(x="9", a="2"))
-    print(sous_function(x="9", lst=[2, 1]))
+    sous(x=9, lst=[2, 1])
+    print(power_function(x=9, a=2))
+    print(add_function(x=9, a=2))
+    print(sous_function(x=9, lst=[2, 1]))
