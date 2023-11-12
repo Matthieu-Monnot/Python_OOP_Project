3dd1adeb25d3f8d17e87b7608d241ce63511ba5d
Matthieu <matthieumonnot@yahoo.fr>
2023-11-12 16:14:26 +0100

    add argument check in decorator

--- app.py
+++ app.py
@@ -1,33 +1,42 @@
 from fastapi import FastAPI, APIRouter
+import ast
 
 
 my_router = APIRouter()
 app = FastAPI()
 
 
-def fast_api_decorator(route, method):
+def fast_api_decorator(route, method, type_args):
     def decorator(func):
-        def wrapper(*args, **kwargs):
+        def wrapper(**kwargs):
+            # Handle argument type error
+            for value, expected_type in zip(kwargs.values(), type_args):
+                if not isinstance(value, expected_type):
+                    raise TypeError(f"Type d'argument incorrect. Attendu : {expected_type.__name__}, Reçu : {type(value).__name__}")
+
+            # add endpoint to the API
             my_router.add_api_route(path=route, endpoint=func, methods=method)
             app.include_router(my_router)
-            return func(*args, **kwargs)
+            return func(**kwargs)
         return wrapper
     return decorator
 
 
-@fast_api_decorator(route="/power/", method=["GET"])
+@fast_api_decorator(route="/power/", method=["GET"], type_args=[str, str])
 def power_function(x: str, a: str):
     return {f"{x} to the power of {a}": int(x)**int(a)}
 
 
-@fast_api_decorator(route="/add/", method=["GET"])
+@fast_api_decorator(route="/add/", method=["GET"], type_args=[str, str])
 def add_function(x: str, a: str):
     return {f"{x} + {a} equals": int(x) + int(a)}
 
 
-@fast_api_decorator(route="/sous/", method=["GET"])
-def sous_function(x: str, lst):
-    return {f"{x} - {lst[0]} - {lst[1]} equals": int(x) - int(lst[0]) - int(lst[1])}
+@fast_api_decorator(route="/sous/", method=["GET"], type_args=[str, list])
+def sous_function(x, lst):
+    x = ast.literal_eval(x)
+    # lst = ast.literal_eval(lst)
+    return {f"{x} - {lst[0]} - {lst[1]} equals": x - lst[0] - lst[1]}
 
 
 # On "lance" les fonctions pour qu'elles soient lisibles par l'app FastAPI
--- main.py
+++ main.py
@@ -31,14 +31,14 @@ def add(x, a):
     print(requests.get(f"http://127.0.0.1:8000/add/?x={x}&a={a}").json())
 
 
-def sous(x, a, b):
-    print(requests.post(f"http://127.0.0.1:8000/sous/?x={x}&a={a}&b={b}").json())
+def sous(x, lst):
+    print(requests.get(f"http://127.0.0.1:8000/sous/?x={x}&lst=[{lst[0]},{lst[1]}]").json())
 
 
 if __name__ == "__main__":
     power(x=9, a=2)
     add(x=9, a=2)
-    sous(x=9, a=2, b=1)
+    #sous(x=9, lst=[2, 1])
     print(power_function(x="9", a="2"))
     print(add_function(x="9", a="2"))
-    print(sous_function(x="9", a="2", b="1"))
+    print(sous_function(x="9", lst=[2, 1]))
